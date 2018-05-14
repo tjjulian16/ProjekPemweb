dac21c097f678de5bc1a6c082b7bafe223113ea8
Tj_Julian <timothy_julian@ymail.com>
2018-05-14 15:16:37 +0700

    login udah bener

--- login.php
+++ login.php
@@ -31,8 +31,7 @@ session_start();
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/jquery.js"></script>
-	<script src="js/script.js"></script>
+
 
 
 </head>
@@ -70,7 +69,7 @@ session_start();
      <div class="col-md-3 col-sm-5 col-xs-5" style="margin: 2% 38%;">
   <div class ="card">
     <div class="card-body">
-        <form  id ="login" method="POST" >
+        <form  action ="autentikasi.php" id ="login" method="POST" >
   <div class="form-group"> 
     <label >Username: </label>
     <input type="text" id="username" class="form-control" placeholder="Masukkan Username" name="Username" required>
